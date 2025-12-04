6be3ab66762a0f369899e9abb31a96fce827680c
Dejvidek16 <dejvid362@gmail.com>
2025-12-04 13:26:52 +0100

    Revert "test api server aby fungoval na tour de app"
    This reverts commit b817fb11ac18b692dfe7eb0f2ecf9c7d9645862b.

--- apps/server/src/routes/users.ts
+++ apps/server/src/routes/users.ts
@@ -4,18 +4,11 @@ import { pool } from "@/db";
 export const userRoutes = express.Router();
 
 userRoutes.get("/", async (_req, res) => {
-	/*try {
+	try {
 		const [rows] = await pool.execute("SELECT * FROM users");
 		res.status(200).json(rows);
 	} catch (error) {
 		console.error("Error fetching users:", error);
 		res.status(500).json({ error: "Failed to fetch users" });
 	}
-		*/
-	const fakeUsers = [
-        { id: 1, name: "Jan Novák", email: "jan@example.com" },
-        { id: 2, name: "Petr Svoboda", email: "petr@example.com" }
-    ];
-
-    res.status(200).json(fakeUsers);
 });
